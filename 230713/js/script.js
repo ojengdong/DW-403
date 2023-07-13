f0d304ae09b947f7633635c8b680f5915d291463
ojengdong <dutkdgus159@naver.com>
2023-07-13 13:58:54 +0900

    23.0713javascript오전수업, homework수정

---
+++ 230713/js/script.js
@@ -0,0 +1,70 @@
+// function hello(name, lang) {
+//     if(lang == "ko") {
+//        alert(name + "님 안녕하세요."); 
+//     }else if (lang == "ge") {
+//        alert(name + "Guten tag."); 
+//     }else if(lang == "ch"){
+//        alert(name + "Ni hao."); 
+//     }
+// }
+
+// const { doc } = require("prettier");
+
+// hello("홍길동", "ge");
+
+// var cel = "유재석";
+
+// function aaa() {
+//     var cel = "한지호"
+//     alert("가장유명한 사람 : " + cel);
+// }
+
+// aaa();
+
+// var a = prompt("첫 번째 수");
+// var b = prompt("두 번째 수");
+
+// alert(a+b)
+
+// document.onmousemove = function(e) {
+//     var x = e.clientX;
+//     var y = e.clientY;
+//     console.log( x + "," + y );
+// }
+
+// 변수 s에 0을 넣어둔다.
+// 문서에서 아이디가 aaa인 녀석을 클릭했을 때
+//  이런일이 벌어질 것이다.
+// s를 1증가시키자
+// 문서에서 아이디가 score인 녀석의 안쪽에 s를 넣자.
+// var s = 1; 
+// document.getElementById("aaa").onclick = function(){
+//     // s++
+//     // s = s + 10;
+//     // s += 1;
+//     // s = s * 2
+//     s *= 2;
+//     document.getElementById("score").innerHTML = s;
+// }
+
+// alert(5 > 3 && 6 > 9);
+
+// var age = 13;
+// var height = 132;
+// var ticket = true;
+// if((age >= 15 || height >= 150) && ticket) {
+//     alert("입장 가능");
+// }else {
+//     alert("입장 불가능");
+// }
+
+// var deposit = 2000000;
+// var monthly = 500;
+// var distance = 15;
+// var top = true;
+// var direction = "south";
+// if ((deposit <= 1000000 && monthly <= 500 && distance <= 15 && top) || direction){
+//     alert("입주");
+// }else {
+//     alert("입주 안함");
+// }
